8b572e84efc6165210ade0d81307fea90967247e
RothaSAMON <samonrotha@gmail.com>
2025-01-07 20:06:04 +0700

    Authentication, Login/Sign Up, assign JWT

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,56 @@
+// import { Module } from '@nestjs/common';
+// import { JwtModule } from '@nestjs/jwt';
+// import { AuthController } from './auth.controller';
+// import { AuthService } from './auth.service';
+// import { PassportModule } from '@nestjs/passport';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { UserSchema } from './schema/user.schema';
+// import { ConfigService } from '@nestjs/config';
+
+// @Module({
+//   imports: [
+//     PassportModule.register({ defaultStrategy: 'jwt' }),
+//     JwtModule.registerAsync({
+//       inject: [ConfigService],
+//       useFactory: (config: ConfigService) => {
+//         return {
+//           secret: config.get('JWT_SECRET'),
+//           signOptions: {
+//             expiresIn: config.get<string | number>('JWT_EXPIRES'),
+//           },
+//         };
+//       },
+//     }),
+//     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+//   ],
+//   controllers: [AuthController],
+//   providers: [AuthService],
+// })
+// export class AuthModule {}
+
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserSchema } from './schema/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: config.get('JWT_EXPIRES') },
+      }),
+    }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { Book } from './schema/book.schema';
